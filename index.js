74d40944581158a7ab252e3b73de862fc0a164ed
c5consulting <c5consulting@yahoo.com>
2018-12-11 05:43:45 -0600

    create simple web server with fastify add car controller and routing TODO: integrate swagger

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
+// Require external modules
+const mongoose = require('mongoose')
+
+// Connect to DB
+mongoose.connect('mongodb://localhost/mycargarage')
+    .then(() => console.log('MongoDB connected…'))
+.catch(err => console.log(err))
+
+
 // Require the framework and instantiate it
 const fastify = require('fastify')({
     logger: true
+})
+
+
+// Import Swagger Options
+const swagger = require('./config/swagger')
+// Register Swagger
+fastify.register(require('fastify-swagger'), swagger.options)
+
+// await fastify.listen(3000)
+// fastify.swagger()
+// fastify.log.info('listening on ${fastify.server.address().port}')
+
+
+const routes = require('./routes')
+routes.forEach((route, index) => {
+    fastify.route(route)
 })
 
 // Declare a route
